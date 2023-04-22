41fc0c3e2b360ef4e71441616b2aa583dbfccfbd
MrMarshall <marcelrenders@live.nl>
2023-04-22 21:08:45 +0200

    Initial commit

---
+++ orbea_stock.py
@@ -0,0 +1,38 @@
+import requests
+
+# Create class to download Orbea stock
+class OrbeaStock:
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+        self.session = requests.Session()
+
+    def login(self):
+        login_url = "https://www.orbea.com/nl-en/kide/login/"
+        login_data = {
+            "login_email": self.email,
+            "login_password": self.password,
+            "from": "",
+            "login": ""
+        }
+        login_response = self.session.post(
+            login_url,
+            data=login_data,
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            allow_redirects=False
+        )
+
+        if login_response.status_code != 303:
+            raise Exception(f"Error: login returned status code [{login_response.status_code}], expected 303")
+
+    def download(self):
+        download_url = "https://www.orbea.com/nl-en/kide/available/csv/"
+        download_response = self.session.get(
+            download_url,
+            headers={"Content-Type": "application/x-www-form-urlencoded"}
+        )
+
+        if download_response.status_code != 200:
+            raise Exception(f"Error: csv download returned status code [{download_response.status_code}], expected 200")
+
+        return download_response.content
