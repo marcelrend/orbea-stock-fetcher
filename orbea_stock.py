13b63b8ee7bfda4e3b3f881a28aef12463804b3d
MrMarshall <marcelrenders@live.nl>
2024-04-26 18:40:38 +0200

    Fix Orbea login by adding more session headers

--- orbea_stock.py
+++ orbea_stock.py
@@ -9,6 +9,18 @@ class OrbeaStock:
         self.session = requests.Session()
         self.session.headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+            "Accept-Language": "nl-NL,nl;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Cache-Control": "no-cache",
+            "Pragma": "no-cache",
+            "Sec-Ch-Ua": '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": "Windows",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Upgrade-Insecure-Requests": "1",
         }
 
     def login(self):
@@ -19,7 +31,7 @@ class OrbeaStock:
             "from": "",
             "login": "",
         }
-        self.session.get(login_url)
+        test = self.session.get(login_url)
         orbea_login_response = self.session.post(
             login_url,
             data=login_data,
